b1095cd0e204e76ad23d4066fe4a741472705576
Keerthan <40045500+KEERTHAN28897@users.noreply.github.com>
2022-09-03 05:08:32 +0530

    Time: 2 ms (59.57%), Space: 49.8 MB (10.72%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -4,56 +4,6 @@ class Solution {
         int lmax[] = new int[n];
         int rmax[] = new int[n];
         
-        Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i < n; ++i) {
-            if(stack.isEmpty()) {
-                stack.add(height[i]);
-                lmax[i] = height[i];
-            } else {
-                int val = stack.peek();
-                if(height[i] < val) {
-                    lmax[i] = val;
-                    stack.add(height[i]);
-                } else {
-                    while(height[i] > val) {
-                        stack.pop();
-                        if(stack.isEmpty()) {
-                            val = height[i];
-                            break;
-                        }
-                        val = stack.peek();
-                    }
-                    lmax[i] = val;
-                    stack.add(height[i]);
-                }
-            }
-        }
-        
-        
-        for(int i = n-1; i > 0; --i) {
-            if(stack.isEmpty()) {
-                stack.add(height[i]);
-                rmax[i] = height[i];
-            } else {
-                int val = stack.peek();
-                if(height[i] < val) {
-                    rmax[i] = val;
-                    stack.add(height[i]);
-                } else {
-                    while(height[i] > val) {
-                        stack.pop();
-                        if(stack.isEmpty()) {
-                            val = height[i];
-                            break;
-                        }
-                        val = stack.peek();
-                    }
-                    rmax[i] = val;
-                    stack.add(height[i]);
-                }
-            }
-        }
-        
         lmax[0] = height[0];
         for(int i = 1; i < n; i++) {
             lmax[i] = Math.max(lmax[i-1], height[i]);
