f9ae0ff0f4f395542dfddcdd52f5fa09cacf63a5
Keerthan <40045500+KEERTHAN28897@users.noreply.github.com>
2023-03-24 11:14:22 +0530

    Time: 50 ms (23.33%), Space: 49.6 MB (15.79%) - LeetHub

---
+++ 0370-range-addition/0370-range-addition.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] getModifiedArray(int length, int[][] updates) {
+    
+        int arr[] = new int[length];
+        for(int i = 0; i < updates.length; i++) {
+            arr[updates[i][0]] += updates[i][2];
+            int end = updates[i][1];
+            System.out.println(end);
+            if(end < (length-1)) {
+                System.out.println(arr[end+1]);
+                arr[end+1] -= updates[i][2];
+            }   
+        }
+        
+        for(int i = 1; i < length; i++) {
+            arr[i] = arr[i] + arr[i-1];
+        }
+        return arr;
+    }
+}
