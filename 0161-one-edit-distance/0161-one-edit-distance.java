4d0dba1b203946d2997927299d291a1056faaea6
Keerthan <40045500+KEERTHAN28897@users.noreply.github.com>
2023-03-31 11:11:07 +0530

    Time: 1382 ms (5.19%), Space: 207.1 MB (5.39%) - LeetHub

---
+++ 0161-one-edit-distance/0161-one-edit-distance.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isOneEditDistance(String s, String t) {
+        int rows = s.length() + 1; int cols = t.length() + 1;
+        int[][] arr = new int[s.length() + 1][t.length() + 1];
+        
+        for(int i = 0; i < cols; i++) {
+            arr[0][i] = i;
+        }
+        
+        for(int j = 0; j < rows; j++) {
+            arr[j][0] = j;
+        }
+        
+        
+        for(int i = 1; i < rows; i++) {
+            for(int j = 1; j < cols; j++) {
+                if(s.charAt(i-1) == t.charAt(j-1)) {
+                    arr[i][j] = arr[i-1][j-1];
+                } else {
+                    arr[i][j] = Math.min(Math.min(arr[i-1][j], arr[i][j-1]), arr[i-1][j-1]) + 1;
+                }
+            }
+        } 
+        
+        System.out.println(arr[rows-1][cols-1]);
+        
+        if(arr[rows-1][cols-1] != 1) {
+            return false;
+        }
+        
+        return true;
+        
+        
+    }
+}
