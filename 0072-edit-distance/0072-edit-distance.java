ec9c5316add29bd69956241b3e3531bfde85fd28
Keerthan <40045500+KEERTHAN28897@users.noreply.github.com>
2023-03-31 11:12:09 +0530

    Time: 6 ms (71.19%), Space: 42.7 MB (35.26%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minDistance(String s, String t) {
+        
+             int rows = s.length() + 1; int cols = t.length() + 1;
+        int[][] arr = new int[s.length() + 1][t.length() + 1];
+        
+        for(int i = 0; i < cols; i++) {
+            arr[0][i] = i;
+        }
+        
+        for(int j = 0; j < rows; j++) {
+            arr[j][0] = j;
+        }
+        
+        
+        for(int i = 1; i < rows; i++) {
+            for(int j = 1; j < cols; j++) {
+                if(s.charAt(i-1) == t.charAt(j-1)) {
+                    arr[i][j] = arr[i-1][j-1];
+                } else {
+                    arr[i][j] = Math.min(Math.min(arr[i-1][j], arr[i][j-1]), arr[i-1][j-1]) + 1;
+                }
+            }
+        } 
+                
+      
+        
+        return arr[rows-1][cols-1];
+        
+    }
+}
+
