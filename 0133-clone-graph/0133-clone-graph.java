a5c95045b0f78e420045555d5fb917ba3e9722b4
Keerthan <40045500+KEERTHAN28897@users.noreply.github.com>
2023-04-08 16:34:26 +0530

    Time: 31 ms (5.77%), Space: 42.8 MB (31.57%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    
+    public Node dfs(Node node, Map<Integer, Node> visited) {
+        
+        if(node == null) {
+            return null;
+        }
+        
+        if(visited.containsKey(node.val)) {
+            return visited.get(node.val);
+        }
+        
+        System.out.print(node.val + " ");
+        
+        List<Node> neighbors = node.neighbors;
+        ArrayList<Node> cloneNeighbors = new ArrayList<>();
+        
+        Node clone = new Node(node.val, cloneNeighbors);
+        visited.put(node.val, clone);
+        
+        for(Node e : neighbors) {
+            Node copy = dfs(e, visited);
+            if(copy != null) {
+                cloneNeighbors.add(copy);
+                
+            }
+        }
+        
+        return clone;
+    }
+    
+    public Node cloneGraph(Node node) {
+        
+        return dfs(node, new HashMap<>());
+    }
+}
