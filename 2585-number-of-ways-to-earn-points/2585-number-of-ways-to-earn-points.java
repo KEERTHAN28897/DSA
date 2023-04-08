c21352cfc62c891df38e787cc3b216aa78c2af58
Keerthan <40045500+KEERTHAN28897@users.noreply.github.com>
2023-04-08 07:29:38 +0530

    Time: 439 ms (11.79%), Space: 60.5 MB (5.05%) - LeetHub

---
+++ 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.java
@@ -0,0 +1,48 @@
+class Solution {
+    int mod = (int)Math.pow(10, 9) + 7;
+    public int rec(int target, int[][] types, int i, int j, int[][][] mem) {
+        if (i == types.length || j > types[i][0])
+            return 0;
+        
+        if (target == 0) {
+             return 1;
+        }
+           
+        
+        if(target < 0)
+            return 0;
+        
+        
+        
+        if(mem[target][i][j] != -1) 
+            return mem[target][i][j];
+        
+        int val = types[i][1];
+        
+        
+        int n1 = 0, n2 = 0;
+   
+        n1 = rec(target - val, types, i, j+1, mem) % mod;
+        n2 = rec(target, types, i+1, 0, mem) % mod;
+
+        
+        int ans = n1 + n2;
+        mem[target][i][j] = ans % mod;
+        return ans % mod;
+    }
+    
+    
+    
+    public int waysToReachTarget(int target, int[][] types) {
+        int[][][] mem = new int[target+1][51][51];
+        for(int i = 0; i < target+1; i++) {
+            for(int j = 0; j < 51; j++) {
+                for(int k = 0; k < 51; k++) {
+                    mem[i][j][k] = -1;
+                }
+            }  
+        }
+        
+        return rec(target, types, 0, 0, mem);
+    }
+}
